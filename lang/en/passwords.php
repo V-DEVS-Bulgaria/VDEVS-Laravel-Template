738a1eea8a7b4385eed47e40a113257d3e54d1ad
Vladislav <42325421+dayofpay@users.noreply.github.com>
2023-02-08 05:25:47 +0200

    Update passwords.php

--- lang/en/passwords.php
+++ lang/en/passwords.php
@@ -13,10 +13,10 @@ return [
     |
     */
 
-    'reset' => 'Your password has been reset!',
-    'sent' => 'We have emailed your password reset link!',
-    'throttled' => 'Please wait before retrying.',
-    'token' => 'This password reset token is invalid.',
-    'user' => "We can't find a user with that email address.",
+    'reset' => 'Вашата парола беше занулена!',
+    'sent' => 'На имейла ви изпратихме инструкции за възстановяване на акаунта ви!',
+    'throttled' => 'Моля, изчакайте.',
+    'token' => 'Token-а изтече, опитайте отново.',
+    'user' => "Няма потребител с такъв имейл.",
 
 ];
