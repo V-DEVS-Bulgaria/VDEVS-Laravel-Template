66ae6bb4eb34cf43d6289925ff0ca0e7e123f7fa
Vladislav <42325421+dayofpay@users.noreply.github.com>
2023-02-08 05:25:16 +0200

    Update auth.php

--- lang/en/auth.php
+++ lang/en/auth.php
@@ -13,8 +13,8 @@ return [
     |
     */
 
-    'failed' => 'These credentials do not match our records.',
-    'password' => 'The provided password is incorrect.',
-    'throttle' => 'Too many login attempts. Please try again in :seconds seconds.',
+    'failed' => 'Въведохте грешен имейл или парола, опитайте отново.',
+    'password' => 'Въведената парола е невярна, опитайте отново.',
+    'throttle' => 'Прекалено много опити за вход, опитайте отново след :seconds секунди.',
 
 ];
